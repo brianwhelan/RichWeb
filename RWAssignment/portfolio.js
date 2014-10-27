002e3d4344c3168c1869aed4f14f8262d324c2d1
Brian <kardkontrol@gmail.com>
2014-10-27 17:07:18 +0000

    Added hover over on picture and created other html docs which need to populated with data

--- RWAssignment/portfolio.js
+++ RWAssignment/portfolio.js
@@ -1,7 +1,18 @@
 function grow() {
 	
-	document.getElementById("more_info").innerHTML =
-	"WUT?"
-
-
+	document.getElementById("content").innerHTML =
+		"WOOP WOOP"
+	
 }
+
+function change_picture(my_image){
+	
+	my_image.src = 'http://www.doggish.com.au/assets/11237_326.jpg';
+}
+
+function revert_picture(my_image){
+	
+	my_image.src = 'http://beautyinfozone.com/wp-content/uploads/2014/07/big-smile.jpeg';
+}
+
+//comments for later use
